940b6da30ed64477a19b88ad4ad4e3576c2e2020
Kevin Baragona <kmbaragona@gmail.com>
2022-02-09 13:23:51 -0800

    verison 1.0.18 - update dependencies, dramatically clean up code

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require('./lib/deepai');
+module.exports = require("./lib/deepai");
--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,75 @@
-var webpack = require('webpack');
+var webpack = require("webpack");
 var config = {};
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const path = require('path');
+const path = require("path");
 
 function generateConfig(name) {
-    var uglify = name.indexOf('min') > -1;
+  var uglify = name.indexOf("min") > -1;
 
-    var config = {
-        entry: ['@babel/polyfill','./index.js'],
-        output: {
-            path:  path.resolve(__dirname, 'dist/') ,
-            filename: name + '.js',
-            sourceMapFilename: name + '.map',
-            library: 'deepai',
-            libraryTarget: 'umd'
+  var config = {
+    entry: ["regenerator-runtime/runtime.js", "./index.js"],
+    output: {
+      path: path.resolve(__dirname, "dist/"),
+      filename: name + ".js",
+      sourceMapFilename: name + ".map",
+      library: {
+        name: "deepai",
+        type: "umd",
+        umdNamedDefine: true, // Important
+        export: "default",
+      },
+    },
+    node: {},
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.js?$/, // include .js files
+          enforce: "pre", // preload the jshint loader
+          exclude: /(node_modules)/, // exclude any and all files in the node_modules folder
+          use: [
+            {
+              loader: "babel-loader",
+              // more options in the optional jshint object
+              options: {},
+            },
+          ],
         },
-        node: {
-            process: false
-        },
-        devtool: 'source-map',
-        module: {
-            rules: [{
-              test: /\.js?$/, // include .js files
-              enforce: "pre", // preload the jshint loader
-              exclude: /(node_modules)/, // exclude any and all files in the node_modules folder
-              use: [{
-                loader: "babel-loader",
-                // more options in the optional jshint object
-                options: {
+      ],
+    },
+    optimization: {
+      // We no not want to minimize our code.
+      minimize: uglify,
+    },
+  };
 
-                }
-              }]
-            }]
-        },
-        optimization: {
-            // We no not want to minimize our code.
-            minimize: uglify
-	    }
-    };
+  config.plugins = [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+    }),
+  ];
+  //
+  //    if (uglify) {
+  //        config.plugins.push(
+  //        new UglifyJsPlugin({
+  //            uglifyOptions: {
+  //                warnings: true,
+  //                ie8: true,
+  //                mangle:false,
+  //                output: {
+  //                    comments: false
+  //                }
+  //                }
+  //            })
+  //        );
+  //    }
 
-    config.plugins = [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-        })
-    ];
-//
-//    if (uglify) {
-//        config.plugins.push(
-//        new UglifyJsPlugin({
-//            uglifyOptions: {
-//                warnings: true,
-//                ie8: true,
-//                mangle:false,
-//                output: {
-//                    comments: false
-//                }
-//                }
-//            })
-//        );
-//    }
+  config.mode = "production";
 
-    config.mode = 'production';
-
-    return config;
+  return config;
 }
 
-['deepai', 'deepai.min'].forEach(function (key) {
-    config[key] = generateConfig(key);
+["deepai", "deepai.min"].forEach(function (key) {
+  config[key] = generateConfig(key);
 });
 
 module.exports = config;
