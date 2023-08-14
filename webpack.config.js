fb9998f72673caeef9207999a71f68a937e612aa
DJ Stomp <85457381+DJStompZone@users.noreply.github.com>
2023-08-14 13:31:02 -0700

    Refactor: Separate builds for browser/node
    Introduced a structural update to the Webpack config to generate distinct builds for the browser and Node.js environments to ensure compatibility and functionality on all supported platforms

--- webpack.config.js
+++ webpack.config.js
@@ -1,93 +1,58 @@
 var webpack = require("webpack");
-var config = {};
 const path = require("path");
 
-function generateConfig(name) {
-  var uglify = name.indexOf("min") > -1;
-
-  var config = {
-    entry: ["regenerator-runtime/runtime.js", "./index.js"],
-    target: "web",
-    // resolve: {
-    //     fallback: {
-    //       "fs": false,
-    //       "tls": false,
-    //       "net": false,
-    //       "path": false,
-    //       "zlib": false,
-    //       "http": false,
-    //       "https": false,
-    //       "stream": false,
-    //       "crypto": false,
-    //       "crypto-browserify":false,
-    //       "util": require.resolve("util/")
-    //     }
-    //   },
-    output: {
-      path: path.resolve(__dirname, "dist/"),
-      filename: name + ".js",
-      sourceMapFilename: name + ".map",
-      globalObject: "this",
-
-      library: {
-        name: "deepai",
-        type: "umd",
-        // umdNamedDefine: true, // Important
-        // export: "default",
-      },
-    },
-    node: {},
-    devtool: "source-map",
-    module: {
-      rules: [
-        {
-          test: /\.js?$/, // include .js files
-          enforce: "pre", // preload the jshint loader
-          exclude: /(node_modules)/, // exclude any and all files in the node_modules folder
-          use: [
-            {
-              loader: "babel-loader",
-              // more options in the optional jshint object
-              options: {},
+function generateConfig(name, target) {
+    var uglify = name.indexOf("min") > -1;
+  
+    var config = {
+        entry: ["regenerator-runtime/runtime.js", "./index.js"],
+        target: target,
+        output: {
+            path: path.resolve(__dirname, "dist/"),
+            filename: name + ".js",
+            sourceMapFilename: name + ".map",
+            globalObject: "this",
+            library: {
+                name: "deepai",
+                type: "umd",
             },
-          ],
         },
-      ],
-    },
-    optimization: {
-      // We no not want to minimize our code.
-      minimize: uglify,
-    },
-  };
-
-  config.plugins = [
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
-    }),
-  ];
-  //
-  //    if (uglify) {
-  //        config.plugins.push(
-  //        new UglifyJsPlugin({
-  //            uglifyOptions: {
-  //                warnings: true,
-  //                ie8: true,
-  //                mangle:false,
-  //                output: {
-  //                    comments: false
-  //                }
-  //                }
-  //            })
-  //        );
-  //    }
-
-  config.mode = "production";
-
-  return config;
+        node: {},
+        devtool: "source-map",
+        module: {
+            rules: [
+                {
+                    test: /\.js?$/,
+                    enforce: "pre",
+                    exclude: /(node_modules)/,
+                    use: [
+                        {
+                            loader: "babel-loader",
+                            options: {},
+                        },
+                    ],
+                },
+            ],
+        },
+        optimization: {
+            minimize: uglify,
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+            }),
+        ],
+        mode: "production",
+    };
+  
+    return config;
 }
 
-["deepai", "deepai.min"].forEach(function (key) {
-  config[key] = generateConfig(key);
+const configurations = [];
+
+["deepai", "deepai.min"].forEach(function(key) {
+    configurations.push(generateConfig(key, 'web'));   // Browser version
+    configurations.push(generateConfig(key + '.node', 'node'));   // Node.js version
 });
 
-module.exports = config;
+module.exports = configurations;
